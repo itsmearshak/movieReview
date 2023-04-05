046c20a9cc655fa0843a0b037dcec75502352dec
arshak <arshakkp05@gmail.com>
2023-04-05 20:42:10 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'home',component:HomeComponent},
+  {path:'movie',component:MovieComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
