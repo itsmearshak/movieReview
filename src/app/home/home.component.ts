8289581e1c953cb919957301641c3ee67433bdfb
Arshakkp <arshakkpfldev@gmail.com>
2023-04-07 21:28:59 +0530

    all done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import{HttpClient} from '@angular/common/http'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +9,32 @@ import{HttpClient} from '@angular/common/http'
 })
 export class HomeComponent {
   trendingMovies :any;
-constructor(private http:HttpClient){}
+  theatreMovies:any;
+  popularMovies:any;
+constructor(private http:HttpClient , private router :Router){}
 ngOnInit():void{
   this.getTrendingMovie();
+  this.getTheaterMovie();
+  this.getPopularMovie();
 }
 getTrendingMovie(){
   this.http.get('http://localhost:4200/assets/data/trending-movie.json').subscribe((movie)=>{
 this.trendingMovies=movie; 
 console.log(this.trendingMovies);
   })
+}
+getTheaterMovie(){
+  this.http.get('http://localhost:4200/assets/data/theatre-movie.json').subscribe((movie)=>{
+this.theatreMovies=movie;
+  })
+}
+getPopularMovie(){
+  this.http.get('http://localhost:4200/assets/data/popular-movie.json').subscribe((movie)=>{
+this.popularMovies=movie;
+console.log(this.popularMovies);
+  })
+}
+gotoMovie(type:string ,id :string){
+this.router.navigate(['movie',type ,id]);
 }
 }
--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent {
+  type = '';
+  id = '';
+  movies: any;
+  movie: any;
+  url = 'http://localhost:4200/assets/data/';
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.type = this.route.snapshot.params['type'];
+    this.id = this.route.snapshot.params['id'];
+
+    this.url += this.type + '.json';
+    console.log(this.url);
+    this.getMovieDetail();
+  }
+
+  getMovieDetail() {
+    this.http.get(this.url).subscribe((movies) => {
+      this.movies = movies;
+      console.log(movies);
+      console.log(this.id);
+
+      let index = this.movies.findIndex((movie: { id: string; }) => {return movie.id.toString() === this.id });
+      console.log(index);
+      if (index > -1) {
+        this.movie = this.movies[index];
+      }
+    });
+  }
 }
